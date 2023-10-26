2cd6655b0100a2ed6e4a79efe06bee67cf8fcf7a
Cristiano <jcristiano.c@gmail.com>
2023-10-26 10:50:46 -0300

    Incluido entrada da doc do Mobile

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'mobileSidebar',
             position: 'left',
             label: 'Mobile',
           },
